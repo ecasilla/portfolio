3c722ae989b532ee58fdc5af32da7912721f2847
Ernie Casilla <ecasilla@icloud.com>
2014-01-01 23:37:42 -0500

    change in direction

--- server.rb
+++ server.rb
@@ -6,22 +6,3 @@ get "/" do
   erb :welcome
 end
 
-get "/about" do
-  erb :about
-end
-
-get "/resume" do
-  erb :resume
-end
-
-get "/contact" do
-  erb :contact
-end
-
-post "/contact" do
-  erb :contact
-end
-
-get "/portfolio" do
-  erb :portfolio
-end
