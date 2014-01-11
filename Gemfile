4fbe9fd83192612629e538c35230ceb8fbb0ff35
Ernie Casilla <ecasilla@icloud.com>
2014-01-11 17:06:11 -0500

    education images

--- Gemfile
+++ Gemfile
@@ -2,6 +2,4 @@ source "https://rubygems.org"
 ruby "2.0.0"
 gem 'sinatra', '1.1.0'
 gem 'font-awesome-sass'
-
-source "http://gems.github.com"
-gem  "sbfaulkner-sinatra-prawn"
+gem 'rails_12factor'
--- server.rb
+++ server.rb
@@ -1,14 +1,6 @@
 require 'rubygems'
 require 'sinatra'
-require 'sinatra/prawn'
-
-set :prawn, { :page_layout => :portrait }
 
 get "/" do
   erb :welcome
-end
-
-get '/pdf' do
-  content_type 'application/pdf'
-  prawn :pdf
 end
